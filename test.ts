9d08d97754c03c70ce5d6e877fd363737d98c041
Dmitriy Antipov <aston_bonny@mail.ru>
2021-04-11 10:46:46 +0300

    Update main.ts, test.ts

--- test.ts
+++ test.ts
@@ -83,3 +83,23 @@ function TestRGBToHSVToColor(colorSensor: sensors.HiTechnicColorSensor) {
         pause(10);
     }
 }
+
+function Main() {
+    TestRGBToHSVToColor(sensors.hitechnicColor1);
+    while (true) {
+        brick.clearScreen();
+        brick.showValue("Color", sensors.hitechnicColor1.getColor(), 2);
+        brick.showValue("R", sensors.hitechnicColor1.getRGB()[0], 3);
+        brick.showValue("G", sensors.hitechnicColor1.getRGB()[1], 4);
+        brick.showValue("B", sensors.hitechnicColor1.getRGB()[2], 5);
+        brick.showValue("W", sensors.hitechnicColor1.getWhite(), 6);
+        brick.showValue("Color", sensors.hitechnicColor4.getColor(), 8);
+        brick.showValue("R", sensors.hitechnicColor4.getRGB()[0], 9);
+        brick.showValue("G", sensors.hitechnicColor4.getRGB()[1], 10);
+        brick.showValue("B", sensors.hitechnicColor4.getRGB()[2], 11);
+        brick.showValue("W", sensors.hitechnicColor4.getWhite(), 12);
+        brick.showValue("Raw", sensors.color2.reflectedLightRaw(), 14);
+        loops.pause(50);
+    }
+}
+Main();
