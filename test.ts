a0748f0722aa6e0291726057d85e5ac0867c594a
Dmitriy Antipov <aston_bonny@mail.ru>
2021-04-14 19:45:06 +0300

    Updates.

--- test.ts
+++ test.ts
@@ -1,12 +1,12 @@
 function Main() {
     while (true) {
         brick.clearScreen();
-        brick.showValue("Color", sensors.hitechnicColor1.getColor(), 2);
-        brick.showValue("R", sensors.hitechnicColor1.getRGB()[0], 3);
-        brick.showValue("G", sensors.hitechnicColor1.getRGB()[1], 4);
-        brick.showValue("B", sensors.hitechnicColor1.getRGB()[2], 5);
-        brick.showValue("W", sensors.hitechnicColor1.getWhite(), 6);
-        brick.showValue("Raw", sensors.color2.reflectedLightRaw(), 14);
+        brick.showValue("Color", sensors.hitechnicColor1.getColor(), 1);
+        brick.showValue("R", sensors.hitechnicColor1.getRGB()[0], 2);
+        brick.showValue("G", sensors.hitechnicColor1.getRGB()[1], 3);
+        brick.showValue("B", sensors.hitechnicColor1.getRGB()[2], 4);
+        brick.showValue("W", sensors.hitechnicColor1.getWhite(), 5);
+        brick.showValue("Raw", sensors.color2.reflectedLightRaw(), 6);
         loops.pause(50);
     }
 }
