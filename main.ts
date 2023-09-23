12141accae5fae9411365586fe8d22c8c9708f22
Dmitriy Antipov <aston_bonny@mail.ru>
2023-09-23 20:31:05 +0300

    update-1.1.9

--- main.ts
+++ main.ts
@@ -3,50 +3,53 @@ namespace sensors {
     export class HiTechnicColorSensor extends sensors.internal.IICSensor {
 
         _query() {
-            this.poke()
-            this.transaction(1, [66], 5)
-            return 0
+            this.transaction(1, [66], 5);
+            return 0;
         }
 
         /**
-         * TODO: Get color code from ht color sensor
+         * Get color code from ht color sensor.
          */
         //% block
         getColor() {
-            return this.getBytes()[0]
+            this.poke();
+            return this.getBytes()[0];
         }
 
         /**
-         * TODO: Get array with RGB values ht color sensor
+         * Get array with RGB values ht color sensor.
          */
         //% block
         getRGB() {
-            return [this.getBytes()[1], this.getBytes()[2], this.getBytes()[3]]
+            this.poke();
+            return [this.getBytes()[1], this.getBytes()[2], this.getBytes()[3]];
         }
 
         /**
-         * TODO: Get white from ht color sensor
+         * Get white from ht color sensor.
          */
         //% block
         getWhite() {
-            return this.getBytes()[4]
+            this.poke();
+            return this.getBytes()[4];
         }
 
         /**
-         * TODO: Get all values from ht color sensor
+         * Get all values from ht color sensor.
          */
         //% block
         getAll() {
-            return this.getBytes()
+            this.poke();
+            return this.getBytes();
         }
 
         _IICId() {
-            return 'HiTechncColorPD'
+            return 'HiTechncColorPD';
         }
     }
 
-    export const hitechnicColor1 = new HiTechnicColorSensor(1)
-    export const hitechnicColor2 = new HiTechnicColorSensor(2)
-    export const hitechnicColor3 = new HiTechnicColorSensor(3)
-    export const hitechnicColor4 = new HiTechnicColorSensor(4)
+    export const hitechnicColor1 = new HiTechnicColorSensor(1);
+    export const hitechnicColor2 = new HiTechnicColorSensor(2);
+    export const hitechnicColor3 = new HiTechnicColorSensor(3);
+    export const hitechnicColor4 = new HiTechnicColorSensor(4);
 }
