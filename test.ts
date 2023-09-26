4d5b4d719e0ccb8339459cc58e59db3c46d9c7a8
Dmitriy Antipov <aston_bonny@mail.ru>
2023-09-26 13:44:45 +0300

    update-1.3.0

--- test.ts
+++ test.ts
@@ -1,14 +1,9 @@
-function Main() {
-    while (true) {
-        brick.clearScreen();
-        brick.showValue("Color", sensors.hitechnicColor1.getColor(), 1);
-        brick.showValue("R", sensors.hitechnicColor1.getRGB()[0], 2);
-        brick.showValue("G", sensors.hitechnicColor1.getRGB()[1], 3);
-        brick.showValue("B", sensors.hitechnicColor1.getRGB()[2], 4);
-        brick.showValue("W", sensors.hitechnicColor1.getWhite(), 5);
-        brick.showValue("Raw", sensors.color2.reflectedLightRaw(), 6);
-        loops.pause(50);
-    }
-}
-
-Main();
+forever(function () {
+    brick.clearScreen()
+    brick.showValue("Color", sensors.htColor4.getColor(), 1)
+    brick.showValue("R", sensors.htColor4.getRGB()[0], 2)
+    brick.showValue("G", sensors.htColor4.getRGB()[1], 3)
+    brick.showValue("B", sensors.htColor4.getRGB()[2], 4)
+    brick.showValue("W", sensors.htColor4.getWhite(), 5)
+    loops.pause(50)
+})
