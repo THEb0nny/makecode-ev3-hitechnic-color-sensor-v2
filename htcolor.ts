e7e32871c03c095808de59a34a1189b9accca38d
Dmitriy Antipov <aston_bonny@mail.ru>
2023-09-26 23:49:11 +0300

    update-1.4.0

--- htcolor.ts
+++ htcolor.ts
@@ -17,66 +17,46 @@ namespace sensors {
         constructor(port: number) {
             super(port);
         }
+        
+        setMode(m: HTColorSensorV2Mode) {
+            this._setMode(m);
+        }
+
+        _setMode(m: number) {
+            let v = m | 0;
+            this.mode = v;
+            if (!this.isActive()) return;
+            if (this.realmode != this.mode) {
+                this.realmode = v;
+                //setIICMode(this._port, this._deviceType(), v);
+            }
+        }
+
+        getMode() {
+            return <HTColorSensorV2Mode>this.mode;
+        }
 
         _query() {
             this.transaction(1, [66], 5);
-            return this.getBytes()[0];
+            if (this.mode == HTColorSensorV2Mode.All) {
+                return [this.getBytes()[0], this.getBytes()[1], this.getBytes()[2], this.getBytes()[3], this.getBytes()[4]];
+            } else if (this.mode == HTColorSensorV2Mode.Color) {
+                return [this.getBytes()[0]];
+            } else if (this.mode == HTColorSensorV2Mode.Rgb) {
+                return [this.getBytes()[1], this.getBytes()[2], this.getBytes()[3]];
+            } else if (this.mode == HTColorSensorV2Mode.White) {
+                return [this.getBytes()[4]];
+            }
+            return [0];
         }
 
-        _queryArr() {
-            this.transaction(1, [66], 5);
-            return [this.getBytes()[0], this.getBytes()[1], this.getBytes()[2], this.getBytes()[3], this.getBytes()[4]];
-        }
-
-        /**
-         * Get color code from HiTechnic Color Sensor v2.
-         * @param sensor the ht color sensor v2 port
-         */
-        //% block="**ht color sensor** $this|color"
-        //% blockId=HTColorSensorV2GetColor
-        //% parts="htcolorsensor"
-        //% blockNamespace=sensors
-        //% this.fieldEditor="ports"
-        //% weight=99 blockGap=12
-        //% subcategory="HiTechnic"
-        //% group="Color Sensor V2"
-        getColor(): number {
-            this.poke();
-            return this.getBytes()[0];
-        }
-
-        /**
-         * Get array with RGB values from HiTechnic Color Sensor v2.
-         * @param sensor the ht color sensor v2 port
-         */
-        //% block="**ht color sensor** $this|rgb"
-        //% blockId=HTColorSensorV2GetRGB
-        //% parts="htcolorsensor"
-        //% blockNamespace=sensors
-        //% this.fieldEditor="ports"
-        //% weight=98 blockGap=12
-        //% subcategory="HiTechnic"
-        //% group="Color Sensor V2"
-        getRGB(): number[] {
-            this.poke();
-            return [this.getBytes()[1], this.getBytes()[2], this.getBytes()[3]];
-        }
-
-        /**
-         * Get white from HiTechnic Color Sensor v2.
-         * @param sensor the ht color sensor v2 port
-         */
-        //% block="**ht color sensor** $this|white"
-        //% blockId=HTColorSensorV2GetWhite
-        //% parts="htcolorsensor"
-        //% blockNamespace=sensors
-        //% this.fieldEditor="ports"
-        //% weight=97 blockGap=12
-        //% subcategory="HiTechnic"
-        //% group="Color Sensor V2"
-        getWhite(): number {
-            this.poke();
-            return this.getBytes()[4];
+        _info() {
+            if (HTColorSensorV2Mode.Color || HTColorSensorV2Mode.White) {
+                return [this._query()[0].toString()];
+            } else if (this.mode == HTColorSensorV2Mode.All || this.mode == HTColorSensorV2Mode.Rgb) {
+                return this._query().map(number => number.toString());
+            }
+            return ["0"];
         }
 
         /**
@@ -92,8 +72,63 @@ namespace sensors {
         //% subcategory="HiTechnic"
         //% group="Color Sensor V2"
         getAll(): number[] {
+            this.setMode(HTColorSensorV2Mode.All);
             this.poke();
-            return [this.getBytes()[0], this.getBytes()[1], this.getBytes()[2], this.getBytes()[3], this.getBytes()[4]];
+            return this._query();
+        }
+
+        /**
+         * Get color code from HiTechnic Color Sensor v2.
+         * @param sensor the ht color sensor v2 port
+         */
+        //% block="**ht color sensor** $this|color"
+        //% blockId=HTColorSensorV2GetColor
+        //% parts="htcolorsensor"
+        //% blockNamespace=sensors
+        //% this.fieldEditor="ports"
+        //% weight=99 blockGap=12
+        //% subcategory="HiTechnic"
+        //% group="Color Sensor V2"
+        getColor(): number {
+            this.setMode(HTColorSensorV2Mode.Color);
+            this.poke();
+            return this._query()[0];
+        }
+
+        /**
+         * Get array with RGB values from HiTechnic Color Sensor v2.
+         * @param sensor the ht color sensor v2 port
+         */
+        //% block="**ht color sensor** $this|rgb"
+        //% blockId=HTColorSensorV2GetRGB
+        //% parts="htcolorsensor"
+        //% blockNamespace=sensors
+        //% this.fieldEditor="ports"
+        //% weight=98 blockGap=12
+        //% subcategory="HiTechnic"
+        //% group="Color Sensor V2"
+        getRGB(): number[] {
+            this.setMode(HTColorSensorV2Mode.Rgb);
+            this.poke();
+            return this._query();
+        }
+
+        /**
+         * Get white from HiTechnic Color Sensor v2.
+         * @param sensor the ht color sensor v2 port
+         */
+        //% block="**ht color sensor** $this|white"
+        //% blockId=HTColorSensorV2GetWhite
+        //% parts="htcolorsensor"
+        //% blockNamespace=sensors
+        //% this.fieldEditor="ports"
+        //% weight=97 blockGap=12
+        //% subcategory="HiTechnic"
+        //% group="Color Sensor V2"
+        getWhite(): number {
+            this.setMode(HTColorSensorV2Mode.White);
+            this.poke();
+            return this._query()[0];
         }
 
         _IICId() {
@@ -103,10 +138,13 @@ namespace sensors {
 
     //% whenUsed block="1" weight=95 fixedInstance jres=icons.port1
     export const htColor1 = new HiTechnicColorSensor(1);
+
     //% whenUsed block="2" weight=90 fixedInstance jres=icons.port2
     export const htColor2 = new HiTechnicColorSensor(2);
+
     //% whenUsed block="3" weight=90 fixedInstance jres=icons.port3
     export const htColor3 = new HiTechnicColorSensor(3);
+    
     //% whenUsed block="4" weight=90 fixedInstance jres=icons.port4
     export const htColor4 = new HiTechnicColorSensor(4);
 }
