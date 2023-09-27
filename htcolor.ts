38cb0179f629a48e7474336896c8f95ee884db9e
Dmitriy Antipov <aston_bonny@mail.ru>
2023-09-27 08:58:13 +0300

    update-1.4.2

--- htcolor.ts
+++ htcolor.ts
@@ -5,6 +5,11 @@ const enum HTColorSensorV2Mode {
     White = 3
 }
 
+const enum HTColorSensorV2Frequency {
+    Sixty = 0,
+    Fifty = 1
+}
+
 namespace sensors {
 
     /**
@@ -122,7 +127,7 @@ namespace sensors {
         //% parts="htcolorsensor"
         //% blockNamespace=sensors
         //% this.fieldEditor="ports"
-        //% weight=97 blockGap=12
+        //% weight=97
         //% subcategory="HiTechnic"
         //% group="Color Sensor V2"
         getWhite(): number {
@@ -132,11 +137,75 @@ namespace sensors {
         }
 
         /**
+         * Get array with HSVL values from HiTechnic Color Sensor v2.
+         * @param sensor the ht color sensor v2 port
+         */
+        //% block="**ht color sensor** $this|hsvl"
+        //% blockId=HTColorSensorV2GetHSVL
+        //% parts="htcolorsensor"
+        //% blockNamespace=sensors
+        //% this.fieldEditor="ports"
+        //% weight=96
+        //% subcategory="HiTechnic"
+        //% group="Color Sensor V2"
+        getHSVL(): number[] {
+            // https://github.com/ofdl-robotics-tw/EV3-CLEV3R-Modules/blob/main/Mods/HTColorV2.bpm
+            this.setMode(HTColorSensorV2Mode.Rgb);
+            this.poke();
+            const rgbArr = this._query();
+            let r = rgbArr[0], g = rgbArr[1], b = rgbArr[2];
+
+            // https://clev3r.ru/codesamples/
+            // Color sensor V2 RGB Maxmium is 255
+            let hue = 0, sat = 0, val = 0;
+            
+            let max = Math.max(r, g);
+            max = Math.max(max, b);
+            let min = Math.min(r, g);
+            min = Math.min(min, b);
+            let light = (max + min) / 5.12;
+            val = max / 2.56;
+            if (val == 0) {
+                hue = -1;
+                sat = -1;
+            }
+
+            r = r / max;
+            g = g / max;
+            b = b / max;
+            max = Math.max(r, g);
+            max = Math.max(max, b);
+            min = Math.min(r, g);
+            min = Math.min(min, b);
+            sat = (max - min) * 100;
+            if (sat == 0) hue = -1;
+
+            r = (r - min) / (max - min);
+            g = (g - min) / (max - min);
+            b = (b - min) / (max - min);
+            max = Math.max(r, g);
+            max = Math.max(max, b);
+            min = Math.min(r, g);
+            min = Math.min(min, b);
+
+            if (max == r) {
+                hue = 0 + 60 + (g - b);
+                if (hue < 0) hue += 360;
+            } else if (max == g) {
+                hue = 120 + 60 * (b - r);
+            } else {
+                hue = 240 + 60 * (r - g);
+            }
+
+            return [Math.round(hue), Math.round(sat), Math.round(val), Math.round(light)];
+        }
+
+        /**
          * Set the sensor to the selected frequency to HiTechnic Color Sensor v2. The Sensor is configured by default for locations with 60Hz electrical supplies so if you live in a country with a 60Hz electrical supply no configuration is necessary.
          * @param sensor the ht color sensor v2 port
          * @param sensor the ht color sensor v2 frequency
          */
-        //% block="**ht color sensor** $this|set $freq|Hz frequency"
+        //% block="**ht color sensor** $this|set $freq|(Hz) frequency"
         //% blockId=HTColorSensorV2SetHz
         //% parts="htcolorsensor"
         //% blockNamespace=sensors
@@ -145,7 +214,9 @@ namespace sensors {
         //% subcategory="HiTechnic"
         //% blockHidden=true
         setHz(freq: HTColorSensorV2Frequency) {
-            
+            // https://github.com/ofdl-robotics-tw/EV3-CLEV3R-Modules/blob/main/Mods/HTColorV2.bpm
+            //Sensor.WriteI2CRegister(port, 1, 65, 53) '50Hz
+            //Sensor.WriteI2CRegister(port, 1, 65, 54) //60Hz
         }
 
         _IICId() {
