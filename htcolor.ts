4a35188987cf45072d3c864fa25b6df01888e993
Dmitriy Antipov <aston_bonny@mail.ru>
2023-09-29 00:15:58 +0300

    update-1.4.4

--- htcolor.ts
+++ htcolor.ts
@@ -1,22 +1,28 @@
-const enum HTColorSensorV2Mode {
-    All = 0,
-    Color = 1,
-    Rgb = 2,
-    Rgbw = 3,
-    White = 4,
-    RawRgbw = 5,
-    PassiveRgbw = 6
+const enum HTCS2Mode {
+    Active = 0, // Use ambient light cancellation
+    Passive = 1, // Disable ambient light cancellation
+    Raw = 3 // Raw data from light sensor
 }
 
-const enum HTColorSensorV2Frequency {
-    Sixty = 0,
-    Fifty = 1
+const enum HTCS2SoftMode {
+    All = 0,
+    Color = 1,
+    Rgbw = 2,
+    PassiveRgbw = 5,
+    RawRgbw = 6,
+}
+
+const enum HTCS2FreqMode {
+    //% block="50"
+    Freq50 = 53, // Set sensor to 50Hz cancellation mode // 35
+    //% block="60"
+    Freq60 = 54 // Set sensor to 60Hz cancellation mode // 36
 }
 
 namespace sensors {
 
     /**
-    * The new and totally redesigned HiTechnic Color Sensor Version 2 (V2) operates by using a single white LED (light-emitting diode) to illuminate the target and analyses the color components of the light reflected by the target's surface and calculates a Color Number that is returned.
+    * The new and totally redesigned HiTechnic Color Sensor V2 operates by using a single white LED to illuminate the target and analyses the color components of the light reflected by the target's surface and calculates a Color Number that is returned.
     * NOTE: The Color Sensor V2 must be configured to match the mains electricity frequency for your country details on how to configure the Color Sensor V2 can be found in the configuration tab on this page.
     */
     //% fixedInstances
@@ -24,9 +30,11 @@ namespace sensors {
 
         constructor(port: number) {
             super(port);
+            this.setMode(HTCS2SoftMode.All);
         }
         
-        setMode(m: HTColorSensorV2Mode) {
+        setMode(m: HTCS2SoftMode) {
+            //this.transaction(1, [65, m], 0);
             this._setMode(m);
         }
 
@@ -36,45 +44,62 @@ namespace sensors {
             if (!this.isActive()) return;
             if (this.realmode != this.mode) {
                 this.realmode = v;
+                if (m == HTCS2SoftMode.All || m == HTCS2SoftMode.Color || m == HTCS2SoftMode.Rgbw) {
+                    this.transaction(1, [65, HTCS2Mode.Active], 0);
+                } else if (m == HTCS2SoftMode.PassiveRgbw) {
+                    this.transaction(1, [65, HTCS2Mode.Passive], 0);
+                } else if (m == HTCS2SoftMode.RawRgbw) {
+                    this.transaction(1, [65, HTCS2Mode.Raw], 0);
+                }
                 //setIICMode(this._port, this._deviceType(), v);
             }
         }
 
-        getMode() {
-            return <HTColorSensorV2Mode>this.mode;
-        }
-
         _query() {
-            this.transaction(1, [66], 5);
-            if (this.mode == HTColorSensorV2Mode.All) {
+            this.transaction(1, [66], 8);
+            if (this.mode == HTCS2SoftMode.All) {
                 return [this.getBytes()[0], this.getBytes()[1], this.getBytes()[2], this.getBytes()[3], this.getBytes()[4]];
-            } else if (this.mode == HTColorSensorV2Mode.Color) {
+            } else if (this.mode == HTCS2SoftMode.Color) {
                 return [this.getBytes()[0]];
-            } else if (this.mode == HTColorSensorV2Mode.Rgb) {
-                return [this.getBytes()[1], this.getBytes()[2], this.getBytes()[3]];
-            } else if (this.mode == HTColorSensorV2Mode.Rgbw) {
+            } else if (this.mode == HTCS2SoftMode.Rgbw) {
                 return [this.getBytes()[1], this.getBytes()[2], this.getBytes()[3], this.getBytes()[4]];
-            } else if (this.mode == HTColorSensorV2Mode.White) {
-                return [this.getBytes()[4]];
+            } else if (this.mode == HTCS2SoftMode.PassiveRgbw || this.mode == HTCS2SoftMode.RawRgbw) {
+                return [this.getBytes()[0], this.getBytes()[1], this.getBytes()[2], this.getBytes()[3], this.getBytes()[4], this.getBytes()[5], this.getBytes()[6], this.getBytes()[7]];
             }
             return [0];
         }
 
         _info() {
-            if (HTColorSensorV2Mode.Color || HTColorSensorV2Mode.White) {
+            if (HTCS2SoftMode.Color) {
                 return [this._query()[0].toString()];
-            } else if (this.mode == HTColorSensorV2Mode.All || this.mode == HTColorSensorV2Mode.Rgb) {
+            } else if (this.mode == HTCS2SoftMode.All || this.mode == HTCS2SoftMode.Rgbw || this.mode == HTCS2SoftMode.PassiveRgbw || this.mode == HTCS2SoftMode.RawRgbw) {
                 return this._query().map(number => number.toString());
             }
             return ["0"];
         }
 
         /**
-         * Get array with all values from HiTechnic Color Sensor v2.
+         * Get software mode to HiTechnic Color Sensor v2.
+         * @param sensor the ht color sensor v2 port
+         */
+        //% block="**ht color sensor** $this|get mode"
+        //% blockId=HTCS2GetMode
+        //% parts="htcolorsensor"
+        //% blockNamespace=sensors
+        //% this.fieldEditor="ports"
+        //% weight=90
+        //% subcategory="HiTechnic"
+        //% blockHidden=true
+        getMode() {
+            return <HTCS2SoftMode>this.mode;
+        }
+
+        /**
+         * Get array with color, red, green, blue, white values from HiTechnic Color Sensor v2.
          * @param sensor the ht color sensor v2 port
          */
         //% block="**ht color sensor** $this|all"
-        //% blockId=HTColorSensorV2GetAll
+        //% blockId=HTCS2GetAll
         //% parts="htcolorsensor"
         //% blockNamespace=sensors
         //% this.fieldEditor="ports"
@@ -82,7 +107,7 @@ namespace sensors {
         //% subcategory="HiTechnic"
         //% group="Color Sensor V2"
         getAll(): number[] {
-            this.setMode(HTColorSensorV2Mode.All);
+            this.setMode(HTCS2SoftMode.All);
             this.poke();
             return this._query();
         }
@@ -92,7 +117,7 @@ namespace sensors {
          * @param sensor the ht color sensor v2 port
          */
         //% block="**ht color sensor** $this|color"
-        //% blockId=HTColorSensorV2GetColor
+        //% blockId=HTCS2GetColor
         //% parts="htcolorsensor"
         //% blockNamespace=sensors
         //% this.fieldEditor="ports"
@@ -100,27 +125,9 @@ namespace sensors {
         //% subcategory="HiTechnic"
         //% group="Color Sensor V2"
         getColor(): number {
-            this.setMode(HTColorSensorV2Mode.Color);
+            this.setMode(HTCS2SoftMode.Color);
             this.poke();
             return this._query()[0];
-        }
-
-        /**
-         * Get array with RGB values from HiTechnic Color Sensor v2.
-         * @param sensor the ht color sensor v2 port
-         */
-        //% block="**ht color sensor** $this|RGB"
-        //% blockId=HTColorSensorV2GetRGB
-        //% parts="htcolorsensor"
-        //% blockNamespace=sensors
-        //% this.fieldEditor="ports"
-        //% weight=98 blockGap=12
-        //% subcategory="HiTechnic"
-        //% group="Color Sensor V2"
-        getRGB(): number[] {
-            this.setMode(HTColorSensorV2Mode.Rgb);
-            this.poke();
-            return this._query();
         }
 
         /**
@@ -128,35 +135,59 @@ namespace sensors {
          * @param sensor the ht color sensor v2 port
          */
         //% block="**ht color sensor** $this|RGBW"
-        //% blockId=HTColorSensorV2GetRGBW
+        //% blockId=HTCS2GetRGBW
+        //% parts="htcolorsensor"
+        //% blockNamespace=sensors
+        //% this.fieldEditor="ports"
+        //% weight=98 blockGap=12
+        //% subcategory="HiTechnic"
+        //% group="Color Sensor V2"
+        getRGBW(): number[] {
+            this.setMode(HTCS2SoftMode.Rgbw);
+            this.poke();
+            return this._query();
+        }
+
+        /**
+         * Get array with RGBW values from HiTechnic Color Sensor v2 when the backlight is off.
+         * @param sensor the ht color sensor v2 port
+         */
+        //% block="**ht color sensor** $this|RGBW at passive"
+        //% blockId=HTCS2GetPassiveRGBW
         //% parts="htcolorsensor"
         //% blockNamespace=sensors
         //% this.fieldEditor="ports"
         //% weight=97 blockGap=12
         //% subcategory="HiTechnic"
         //% group="Color Sensor V2"
-        getRGBW(): number[] {
-            this.setMode(HTColorSensorV2Mode.Rgbw);
-            this.poke();
-            return this._query();
+        getPassiveRGBW(): number[] {
+            this.setMode(HTCS2SoftMode.PassiveRgbw);
+            const r = this.getBytes()[0] * 256 + this.getBytes()[1];
+            const g = this.getBytes()[2] * 256 + this.getBytes()[3];
+            const b = this.getBytes()[4] * 256 + this.getBytes()[5];
+            const w = this.getBytes()[6] * 256 + this.getBytes()[7];
+            return [r, g, b, w];
         }
 
         /**
-         * Get white from HiTechnic Color Sensor v2.
+         * Get array with raw RGBW values from HiTechnic Color Sensor v2.
          * @param sensor the ht color sensor v2 port
          */
-        //% block="**ht color sensor** $this|white"
-        //% blockId=HTColorSensorV2GetWhite
+        //% block="**ht color sensor** $this|raw RGBW"
+        //% blockId=HTCS2GetRawRGBW
         //% parts="htcolorsensor"
         //% blockNamespace=sensors
         //% this.fieldEditor="ports"
-        //% weight=96
+        //% weight=97
         //% subcategory="HiTechnic"
         //% group="Color Sensor V2"
-        getWhite(): number {
-            this.setMode(HTColorSensorV2Mode.White);
-            this.poke();
-            return this._query()[0];
+        getRawRGBW(): number[] {
+            this.setMode(HTCS2SoftMode.RawRgbw);
+            const r = this.getBytes()[0] * 256 + this.getBytes()[1];
+            const g = this.getBytes()[2] * 256 + this.getBytes()[3];
+            const b = this.getBytes()[4] * 256 + this.getBytes()[5];
+            const w = this.getBytes()[6] * 256 + this.getBytes()[7];
+            return [r, g, b, w];
         }
 
         /**
@@ -164,7 +195,7 @@ namespace sensors {
          * @param sensor the ht color sensor v2 port
          */
         //% block="**ht color sensor** $this|HSVL"
-        //% blockId=HTColorSensorV2GetHSVL
+        //% blockId=HTCS2GetHSVL
         //% parts="htcolorsensor"
         //% blockNamespace=sensors
         //% this.fieldEditor="ports"
@@ -173,7 +204,7 @@ namespace sensors {
         //% group="Color Sensor V2"
         getHSVL(): number[] {
             // https://github.com/ofdl-robotics-tw/EV3-CLEV3R-Modules/blob/main/Mods/HTColorV2.bpm
-            this.setMode(HTColorSensorV2Mode.Rgb);
+            this.setMode(HTCS2SoftMode.Rgbw);
             this.poke();
             const rgbArr = this._query();
             let r = rgbArr[0], g = rgbArr[1], b = rgbArr[2];
@@ -181,7 +212,7 @@ namespace sensors {
             // https://clev3r.ru/codesamples/
             // Color sensor V2 RGB Maxmium is 255
             let hue = 0, sat = 0, val = 0;
-            
+
             let max = Math.max(Math.max(r, g), b);
             let min = Math.min(Math.min(r, g), b);
             let light = (max + min) / 5.12;
@@ -190,7 +221,7 @@ namespace sensors {
                 hue = -1;
                 sat = -1;
             }
-            
+
             if (hue != -1 && sat != -1) {
                 r = r / max;
                 g = g / max;
@@ -220,22 +251,21 @@ namespace sensors {
         }
 
         /**
-         * Set the sensor to the selected frequency to HiTechnic Color Sensor v2. The Sensor is configured by default for locations with 60Hz electrical supplies so if you live in a country with a 60Hz electrical supply no configuration is necessary.
+         * Set the sensor to the selected frequency to HiTechnic Color Sensor v2. The Sensor is configured by default for locations with 60Hz electrical supplies.
          * @param sensor the ht color sensor v2 port
          * @param sensor the ht color sensor v2 frequency
          */
         //% block="**ht color sensor** $this|set $freq|(Hz) frequency"
-        //% blockId=HTColorSensorV2SetHz
+        //% blockId=HTCS2SetHz
         //% parts="htcolorsensor"
         //% blockNamespace=sensors
         //% this.fieldEditor="ports"
         //% weight=89 blockGap=12
         //% subcategory="HiTechnic"
-        //% blockHidden=true
-        setHz(freq: HTColorSensorV2Frequency) {
+        setHz(freq: HTCS2FreqMode) {
+            //blockHidden=true
             // https://github.com/ofdl-robotics-tw/EV3-CLEV3R-Modules/blob/main/Mods/HTColorV2.bpm
-            //Sensor.WriteI2CRegister(port, 1, 65, 53) '50Hz
-            //Sensor.WriteI2CRegister(port, 1, 65, 54) //60Hz
+            this.transaction(1, [65, freq], 0);
         }
 
         _IICId() {
