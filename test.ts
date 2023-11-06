f0f359163617897d31ae319d405cecb389da1a2b
Dmitriy Antipov <aston_bonny@mail.ru>
2023-11-06 22:37:07 +0300

    fix-test

--- test.ts
+++ test.ts
@@ -1,9 +1,9 @@
 forever(function () {
     brick.clearScreen()
     brick.showValue("Color", sensors.htColor4.getColor(), 1)
-    brick.showValue("R", sensors.htColor4.getRGB()[0], 2)
-    brick.showValue("G", sensors.htColor4.getRGB()[1], 3)
-    brick.showValue("B", sensors.htColor4.getRGB()[2], 4)
-    brick.showValue("W", sensors.htColor4.getWhite(), 5)
+    brick.showValue("R", sensors.htColor4.getRGBW()[0], 2)
+    brick.showValue("G", sensors.htColor4.getRGBW()[1], 3)
+    brick.showValue("B", sensors.htColor4.getRGBW()[2], 4)
+    brick.showValue("W", sensors.htColor4.getRGBW()[3], 5)
     loops.pause(50)
 })
